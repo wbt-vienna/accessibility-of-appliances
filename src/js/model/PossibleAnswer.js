c210df6a4707ae5e0e5a422636265f1790848a3b
Benjamin Klaus <klaus@technikum-wien.at>
2020-03-27 10:58:32 +0100

    added examples to possible Answer

--- src/js/model/PossibleAnswer.js
+++ src/js/model/PossibleAnswer.js
@@ -3,7 +3,8 @@ import {ObjectModel} from "objectmodel"
 class PossibleAnswer extends ObjectModel({
     id: Number,
     text: String,
-    percentage: Number
+    percentage: Number,
+    examples: [Array] //Array of Objects containing .text property
 }) {}
 
 export {PossibleAnswer};
