a48098daea9f56696ad0e79e06c463a59f74b98c
Benjamin Klaus <klaus@technikum-wien.at>
2020-09-24 14:02:18 +0200

    adapted style using Bootstrap

--- src/vue-components/newSearchView.vue
+++ src/vue-components/newSearchView.vue
@@ -1,30 +1,31 @@
 <template>
-    <div class="wrapper">
+    <div class="container">
         <h2>Neues Gerät erfassen</h2>
         <div>
-            <div class="row">
-                <p class="eleven columns">
-                    Für die Eintragung von Geräten wird die Katalogisierung von <a href="https://geizhals.at/" target="_blank">geizhals.at</a>
-                    verwendet. Suchen Sie nach Kategorie, Hersteller oder Typenbezeichnung um das zu testende Gerät zu finden. Die Eingabe in das Suchfeld startet die Suche automatisch.
-                </p>
+            <div class="form-group">
+                Für die Eintragung von Geräten wird die Katalogisierung von <a href="https://geizhals.at/" target="_blank">geizhals.at</a>
+                verwendet. Suchen Sie nach Kategorie, Hersteller oder Typenbezeichnung um das zu testende Gerät zu finden. Die Eingabe in das Suchfeld startet die Suche automatisch.
             </div>
-
-            <div class="row">
-                <label for="search" class="three columns center">Produktsuche</label>
-                <input id="search" type="search" v-model="query" @input="search()" placeholder="Kategorie, Hersteller, Typenbezeichnung" class="eight columns" autocomplete="off"/>
+            <div class="form-group">
+                <label for="search">Produktsuche</label>
+                <input id="search" type="search" v-model="query" @input="search()" placeholder="Kategorie, Hersteller, Typenbezeichnung" class="form-control" autocomplete="off"/>
             </div>
-            <div class="row" v-if="isSearching">
-                <span class="eight columns offset-by-three">es wird gesucht...</span>
+            <div class="form-group" v-if="isSearching">
+                <span>es wird gesucht...</span>
             </div>
-            <ul style="list-style-type: none" aria-label="Suchergebnisse, die Links in der Liste führen zum Produkt auf geizhals.at und werden in einem neuen Tab geöffnet" class="eight columns offset-by-three">
-                <li v-for="product in searchResults.products" class="row" style="margin-top: 0.5em" >
-                    <div class="nine columns" >
-                        <img :src="product.img" style="margin-right: 1em" aria-hidden="true"/>
-                        <a title="externer Link des Produkts auf geizhals.at in neuem Tab" target="_blank" :href="'https://geizhals.at/' + product.id">{{product.label}}</a>
-                    </div>
-                    <button @click="select(product)" class="three columns" aria-label="Gerät auswählen">Wählen</button>
-                </li>
-            </ul>
+            <div class="">
+                <ul style="list-style-type: none; padding: 0" aria-label="Suchergebnisse, die Links in der Liste führen zum Produkt auf geizhals.at und werden in einem neuen Tab geöffnet">
+                    <li v-for="product in searchResults.products">
+                        <div class="row no-gutters my-4 my-sm-3">
+                            <div class="col-sm-9 mb-2 position-relative" style="min-height: 48px">
+                                <img :src="product.img" style="position: absolute; top: 2px" aria-hidden="true"/>
+                                <a title="externer Link des Produkts auf geizhals.at in neuem Tab" target="_blank" :href="'https://geizhals.at/' + product.id" style="padding-left: 50px; display: block">{{product.label}}</a>
+                            </div>
+                            <button @click="select(product)" class="col-sm-3 form-control btn-primary" aria-label="Gerät auswählen">Wählen</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
--- src/vue-components/newView.vue
+++ src/vue-components/newView.vue
@@ -1,49 +1,48 @@
 <template>
-    <div class="wrapper" v-if="initialized">
+    <div class="container" v-if="initialized">
         <h2 v-if="isNew">Neues Gerät erfassen</h2>
         <h2 v-if="!isNew">Eintrag bearbeiten</h2>
         <div v-if="newEntry && newEntry.product">
-            <div class="row">
-                <h3 class="eleven colums">Gewähltes Produkt</h3>
+            <h3>Gewähltes Produkt</h3>
+            <div class="form-group">
+                <label for="product">Bezeichnung</label>
+                <div id="product" class="row" aria-label="externer Link des gewählten Produkts auf geizhals.at" v-if="newEntry.product">
+                    <a class="col-md-7 col-lg-8 mb-2 mb-md-0" target="_blank" :href="'https://geizhals.at/' + newEntry.product.id">{{newEntry.product.label}}</a>
+                    <div class="col-md-5 col-lg-4">
+                        <button v-if="isNew" @click="resetEntry()" class="form-control btn-primary" title="Anderes Produkt suchen und wählen"><i aria-hidden="true" class="fas fa-times"/> <span>Anderes Produkt wählen</span></button>
+                    </div>
+                </div>
             </div>
-            <div class="row">
-                <label for="product" class="three columns center">Bezeichnung</label>
-                <span id="product" aria-label="externer Link des gewählten Produkts auf geizhals.at" class="seven columns" v-if="newEntry.product">
-                    <a target="_blank" :href="'https://geizhals.at/' + newEntry.product.id">{{newEntry.product.label}}</a>
-                </span>
-                <button v-if="isNew" @click="resetEntry()" class="one column" title="Anderes Produkt suchen und wählen"><i aria-hidden="true" class="fas fa-times"/><span style="display: none" aria-hidden="false">Anderes Produkt suchen und wählen</span></button>
+            <div class="form-group">
+                <label for="category">Kategorie</label>
+                <div id="category" v-if="newEntry.category">{{newEntry.category.label}}</div>
             </div>
-            <div class="row">
-                <label for="category" class="three columns center">Kategorie</label>
-                <span id="category" class="eight columns" v-if="newEntry.category">{{newEntry.category.label}}</span>
-            </div>
-            <div class="row">
-                <h3  class="eleven colums">Allgemeine Informationen</h3>
-            </div>
+
+            <h3 class="mt-4">Allgemeine Informationen</h3>
             <div v-if="newEntry.category">
-                <div class="row">
-                    <label for="displayTypes" class="three columns">Darbietung wesentlicher Informationen für den Gebrauch</label>
-                    <ul role="group" id="displayTypes" class="eight columns">
-                        <li v-for="displayType in constants.DISPLAY_TYPES">
-                            <input :id="displayType" type="checkbox" v-model="newEntry.questionCategories[displayType]"/>
-                            <label :for="displayType" style="display: inline-block">{{displayType + '_CHK' | translate}}</label>
+                <div class="form-group">
+                    <label for="displayTypes">Darbietung wesentlicher Informationen für den Gebrauch</label>
+                    <ul role="group" id="displayTypes">
+                        <li v-for="displayType in constants.DISPLAY_TYPES" class="form-check">
+                            <input :id="displayType" type="checkbox" v-model="newEntry.questionCategories[displayType]" class="form-check-input"/>
+                            <label :for="displayType" class="form-check-label">{{displayType + '_CHK' | translate}}</label>
                         </li>
                     </ul>
                 </div>
-                <div class="row">
-                    <label for="usageTypes" class="three columns">Nutzungsmöglichkeiten des Geräts</label>
-                    <ul role="group" id="usageTypes" class="eight columns">
-                        <li v-for="usageType in constants.USAGE_TYPES">
-                            <input :id="usageType" type="checkbox" v-model="newEntry.questionCategories[usageType]"/>
-                            <label :for="usageType" style="display: inline-block">{{usageType + '_CHK' | translate}}</label>
+                <div class="form-group">
+                    <label for="usageTypes">Nutzungsmöglichkeiten des Geräts</label>
+                    <ul role="group" id="usageTypes">
+                        <li v-for="usageType in constants.USAGE_TYPES" class="form-check">
+                            <input :id="usageType" type="checkbox" v-model="newEntry.questionCategories[usageType]" class="form-check-input"/>
+                            <label :for="usageType" class="form-check-label">{{usageType + '_CHK' | translate}}</label>
                         </li>
                     </ul>
                 </div>
-                <div class="row">
-                    <label for="updatedBy" class="three columns center">Eintrag erstellt von</label>
-                    <input type="text" id="updatedBy" class="eight columns" v-model="newEntry.updatedBy" placeholder="z.B. Namenskürzel / Vorname"/>
+                <div class="form-group">
+                    <label for="updatedBy">Eintrag erstellt von</label>
+                    <input type="text" id="updatedBy" class="form-control" v-model="newEntry.updatedBy" placeholder="z.B. Namenskürzel / Vorname"/>
                 </div>
-                <div v-for="(categoryQuestions, type) in categorizedQuestions" style="margin-top: 3em;">
+                <div v-for="(categoryQuestions, type) in categorizedQuestions" class="mt-4">
                     <div v-if="categoryQuestions.length > 0 && newEntry.questionCategories[type]">
                         <question-list :listQuestions="categoryQuestions" :type="type" :entry="newEntry" :save-attempted="saveAttempted" :choose-answer="chooseAnswer" :add-comment="addComment"/>
                     </div>
@@ -51,26 +50,26 @@
                 <div v-if="anyTypeSelected">
                     <question-list :listQuestions="categorizedQuestions[constants.USAGE_GENERAL]" :type="constants.USAGE_GENERAL" :entry="newEntry" :save-attempted="saveAttempted" :choose-answer="chooseAnswer" :add-comment="addComment"/>
                 </div>
-                <div class="row" v-if="anyTypeSelected" style="display: block">
+                <div class="form-group" v-if="anyTypeSelected" style="display: block">
                     <h3>Kommentar zum Eintrag</h3>
-                    <textarea class="ten columns" type="text" ref="search" id="comment" v-model="newEntry.comment" placeholder="Fügen Sie hier zusätzliche Anmerkungen zum Gerät ein." style="margin-left: 0"/>
+                    <textarea class="form-control" type="text" ref="search" id="comment" v-model="newEntry.comment" placeholder="Fügen Sie hier zusätzliche Anmerkungen zum Gerät ein." style="margin-left: 0"/>
                 </div>
                 <h3 v-if="newEntry.score">Berechnete Bewertungen</h3>
-                <div class="row" v-if="newEntry.score">
-                    <label for="scoreTotal" class="three columns">Aufgrund von Angaben berechnete Gesamtbewertung</label>
-                    <span id="scoreTotal" class="eight columns">{{Math.round(newEntry.score)}} %</span>
+                <div class="form-group" v-if="newEntry.score">
+                    <label for="scoreTotal">Aufgrund von Angaben berechnete Gesamtbewertung</label>
+                    <div id="scoreTotal">{{Math.round(newEntry.score)}} %</div>
                 </div>
-                <div class="row" v-if="newEntry.score">
-                    <label for="scoreTargetgroup" class="three columns">Bewertung je Zielgruppe</label>
-                    <div class="eight columns" id="scoreTargetgroup">
+                <div class="form-group" v-if="newEntry.score">
+                    <label for="scoreTargetgroup">Bewertung je Zielgruppe</label>
+                    <div id="scoreTargetgroup">
                         <div v-for="groupId in Object.keys(newEntry.scoresByGroup)">{{groupId | translate}}: {{Math.round(newEntry.scoresByGroup[groupId])}} %</div>
                     </div>
                 </div>
-                <div v-if="anyTypeSelected" class="row" style="margin-top: 4em">
-                    <button class="five columns offset-by-three button-primary" @click="save()"><i class="fas fa-save"/> Eintrag Speichern</button>
+                <div v-if="anyTypeSelected" class="row no-gutters mt-5">
+                    <button class="form-control btn-primary" @click="save()"><i class="fas fa-save"/> Eintrag Speichern</button>
                 </div>
-                <div class="row" style="margin-bottom: 4em">
-                    <span aria-hidden="true" class="five columns offset-by-three" v-if="anyTypeSelected && !isValid && saveAttempted"><i class="fas fa-times" style="color: red"></i> Fehler! Bitte beantworten Sie alle Fragen! Nicht ausgefüllte Fragen sind rot markiert.</span>
+                <div class="row no-gutters mb-5">
+                    <span aria-hidden="true" v-if="anyTypeSelected && !isValid && saveAttempted"><i class="fas fa-times" style="color: red"></i> Fehler! Bitte beantworten Sie alle Fragen! Nicht ausgefüllte Fragen sind rot markiert.</span>
                     <div class="only-screenreader" v-if="anyTypeSelected">
                         <span aria-live="assertive">{{!isValid && saveAttempted ? 'Fehler! Bitte beantworten Sie alle Fragen! Nicht beantwortete Fragen sind mit Präfix "nicht beantwortet" gekennzeichnet.' : ''}}</span>
                     </div>
@@ -215,30 +214,13 @@
 </script>
 
 <style scoped>
-    h3 {
-        margin-top: 2em;
-    }
-
-    @media (min-width: 550px) {
-        .row {
-            display: flex;
-        }
-
-        .row > label {
-            display: flex;
-        }
-
-        .row > label.center {
-            align-items: center;
-        }
-    }
-
-    .row {
-        margin-bottom: 1em;
-    }
-
     ul {
         list-style-type: none;
         margin-bottom: 0;
+        padding: 0;
+    }
+
+    .form-check-label {
+        font-weight: normal;
     }
 </style>
